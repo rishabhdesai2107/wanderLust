1516a8cb87559eccc071bb1c4b2f5e95bdcaaed3
rishabh <rishabh.desai@gmail.com>
2024-12-19 16:11:27 +0530

    change6

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,25 @@ const {reviewSchema} = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
+const User = require("../models/user.js");
 
 router.get("/signup",(req,res)=>{
-    res.send("form");
+    res.render("users/signup");
 });
+
+router.post("/signup",wrapAsync(async(req,res)=>{
+    try{
+    let {username,email,password} = req.body;
+    const newUser = new User({username,email});
+    await User.register(newUser,password );
+    req.flash("success","User was entered successfully");
+    res.redirect("/listings");
+    }catch(error){
+        req.flash("error",error.message);
+        res.redirect("/signup");
+    }
+    
+}));
 
 
 
